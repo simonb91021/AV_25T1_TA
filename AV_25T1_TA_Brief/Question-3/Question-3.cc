d0613de6b17c4fddd6dbf73ed528951af59721f5
simonb91021 <simon.bayaca01@gmail.com>
2025-03-13 22:02:19 +1100

    Updated Q3 implementation
    - Added comments for self understanding
    - Commented steps
    - CANdata struct
    - Began Parsing of log file function

--- AV_25T1_TA_Brief/Question-3/Question-3.cc
+++ AV_25T1_TA_Brief/Question-3/Question-3.cc
@@ -1,6 +1,10 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
+#include <iomanip>
+#include <vector>
+
 
 // Question 3: This is an extension task that requires you to decode sensor data from a CAN log file.
 // CAN (Controller Area Network) is a communication standard used in automotive applications (including Redback cars)
@@ -20,3 +24,44 @@
 // Resources:
 // https://www.csselectronics.com/pages/can-bus-simple-intro-tutorial
 // https://www.csselectronics.com/pages/can-dbc-file-database-intro
+
+/*
+Extract WheelSpeed RR Vals from Sensors.dbc file's definition of it which are stored in the candump.log file
+Parse thee values correctly
+Store into an output.txt file with the format
+(<UNIX_TIME>): <DECODED_VALUE>
+eg:
+(1705638753.913408): 1234.5
+(1705638754.915609): 6789.0
+...
+// Hint: Think about manual bit masking and shifting, data types required,
+// what formats are used to represent values, etc.
+*/
+
+//BO_ 1797 ECU_WheelSpeed: 8 Vector__XXX,
+
+//  SG_ WheelSpeedRR : 32|16@0+ (0.1,0) [0|0] "km/h" Vector__XXX
+// WheelSpeedRR, Bit starts count from 32, use manual bit masking and shifting for proper extraction of the data for corresponding wheel.
+// Bit length = signal length (16 bits)
+// @0 meaning its big-endian/Motorola
+// +, Unsigned value type
+// (Scale, offset) - Physical val = offset+scale*raw decimal
+
+// Struct for the CAN data
+struct CANdata {
+    double timestamp;
+    uint32_t canId;
+    uint8_t data[8];
+};
+
+//Parse the CAN cata from candump.log to get the corresponding values, do this via a vector of candata.
+std::vector<CANdata> parsingLog(const std::string &logFile) {
+    std::ifstream file(logFile); //to read the log file.
+    std::string canLine; //A string of each line.
+    std::vector<CANdata> data; 
+    while (std::getline(file, canLine)) {
+        CANdata msg{};
+        std::istringstream iss(canLine);
+        
+    }
+}
